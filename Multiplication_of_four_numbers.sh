0b1a144ebfc48bcb757655331869a693654e9a22
root <root@LAPTOP-33N12T6F.>
2025-01-23 08:25:56 -0500

    Multiplication of four numbers

---
+++ Multiplication_of_four_numbers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Value_1=$1
+Value_2=$2
+Value_3=$3
+Value_4=$4
+
+#Multiplication of numbers and print it
+echo $((Value_1 * Value_2 * Value_3 * Value_4))
---
+++ goals.md
@@ -0,0 +1,7 @@
+# Goals
+ 
+
+Multiplication of 4 numbers
+
+
+2 * 3 * 4 = 24
